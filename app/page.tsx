507ebcd186ae93d9964059c63c3abc9ceb6874b2
nearworld <artemismars2@outlook.com>
2024-02-12 15:36:28 +0900

    feat: branching article content with selected state

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,22 @@ export default function Home() {
       <Aside />
       <section className="col-start-2 col-span-5 grid place-items-center">
         <article>
-          <div className="flex flex-col justify-center items-center gap-3">
-            <Image alt="logo" src="/logo.png" width={80} height={80} />
-            <h1 className="text-xl font-bold">선택된 프로젝트가 없습니다.</h1>
-            <span className="font-bold text-gray-500">
-              프로젝트를 선택하시거나 생성하세요.
-            </span>
-            <Button className="mt-2">+ 새 프로젝트 생성하기</Button>
-          </div>
+          {selected ? (
+            <div>
+              <h1 className="">{selected.title}</h1>
+              <p>{selected.description}</p>
+              <span>{selected.dueDate.getDate()}</span>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center items-center gap-3">
+              <Image alt="logo" src="/logo.png" width={80} height={80} />
+              <h1 className="text-xl font-bold">선택된 프로젝트가 없습니다.</h1>
+              <span className="font-bold text-gray-500">
+                프로젝트를 선택하시거나 생성하세요.
+              </span>
+              <Button className="mt-2">+ 새 프로젝트 생성하기</Button>
+            </div>
+          )}
         </article>
       </section>
     </main>
